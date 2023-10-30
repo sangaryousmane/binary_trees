81c7e0cb055b5ae8d6119cb4c5b81684bd1f1618
sangaryousmane <sangaryousmane@gmail.com>
2023-10-30 00:06:39 -0700

    Handle betty style for the task 0

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -3,18 +3,18 @@
 
 /**
  * binary_tree_node - creates a binary tree node
- * parent: the parent node
- * value: the value for the node
+ * @parent: the parent node
+ * @value: the value for the node
  * Return: the node
  */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *newNode;
-       
+
 	newNode = malloc(sizeof(binary_tree_t));
 
 	if (newNode == NULL)
-		return NULL;
+		return (NULL);
 
 	newNode->parent = parent;
 	newNode->n = value;
