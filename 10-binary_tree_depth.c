dc4ede4af1e6ab5bd590dc12c3d3892af5fe8a65
sangaryousmane <sangaryousmane@gmail.com>
2023-10-30 18:25:34 -0700

    Fix betty style

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -13,7 +13,7 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 		return (0);
 	if (tree != NULL && tree->parent)
 	{
-		return 1 + binary_tree_depth(tree->parent);
+		return (1 + binary_tree_depth(tree->parent));
 	}
 	return (0);
 }
